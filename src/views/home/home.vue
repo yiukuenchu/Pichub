aecf96d3f07cfe7b744587b98651187047130b23
yiukuenchu <yiukuenchu@gmail.com>
2018-11-01 14:45:24 +0800

    写了一点顶部nav-bar的代码

---
+++ src/views/home/home.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-row class="container">
+    <el-col :span="24">
+      <div class="nav-logo">
+        <a href="/"><img src="../../assets/camera.png" style="padding-left: 8px;"></a>
+      </div>
+      <div class="nav-title">
+        <el-col :span="24">
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="home">主页</el-menu-item>
+            <el-menu-item index="explore">发现</el-menu-item>
+            <el-menu-item index="home">登录</el-menu-item>
+          </el-menu>
+        </el-col>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+    export default {
+      name: 'home',
+      data() {
+        return {
+          defaultActiveIndex: "/",
+        };
+      },
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
