55057d03f9af256be8128c7af0ca5c8e1e6eb046
yiukuenchu <yiukuenchu@gmail.com>
2018-11-14 16:31:48 +0800

    make some improvements on navigation-bar

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,22 +5,28 @@
 */
 
 <template>
-  <el-row class="container">
-    <el-col :span="24">
-      <div class="nav-logo">
-          <a href="/"><img src="../../assets/camera.png" style="padding-left: 8px;"></a>
-      </div>
-      <div class="nav-title" style="margin-left: 80px">
-        <el-col :span="24">
-          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="explore">发现</el-menu-item>
-            <el-menu-item index="about">关于</el-menu-item>
-            <el-menu-item index="license">版权</el-menu-item>
-          </el-menu>
-        </el-col>
-      </div>
-    </el-col>
-  </el-row>
+  <div>
+    <el-row class="el-tabs__nav">
+      <el-col :span="24">
+        <div class="el-tabs__nav">
+            <a><img src="../../assets/camera.png" width="35" height="35" style="margin-left: 10px" ></a>
+        </div>
+        <div class="el-input" style="margin-left: 20px">
+          <el-input type="text" size="medium"  placeholder="搜索获取高质量图片">
+          </el-input>
+        </div>
+        <div class="el-tabs__nav" style="margin-left: 20px" >
+          <el-col :span="80">
+            <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+              <el-menu-item index="explore">发现</el-menu-item>
+              <el-menu-item index="about">关于</el-menu-item>
+              <el-menu-item index="license">版权</el-menu-item>
+            </el-menu>
+          </el-col>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 
@@ -30,7 +36,7 @@
       data() {
         return {
           defaultActiveIndex: "/",
-        };
+        }
       },
       methods: {
         handleSelect(key, keyPath) {
