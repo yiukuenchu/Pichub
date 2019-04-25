da7c045184371b611e42db66d7c20ee8f1e53380
yiukuenchu <yiukuenchu@gmail.com>
2019-04-25 19:08:37 +0800

    重定向个人博客

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="row">
     <p class="col">Images from <a href="https://unsplash.com" target="_blank" rel="noopener noreferrer">Unsplash</a></p>
-    <h6 class="col">Designed and built by <a href="https://yiukuenchu.github.io/" target="_blank">朱曜锟</a></h6>
+    <h6 class="col">Designed and built by <a href="https://yiukuenchu.me/" target="_blank">朱曜锟</a></h6>
     </div>
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.20/vue.min.js"></script>
